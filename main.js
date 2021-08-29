f0463f73005fdc5306ed0cecc2d341238ef4c513
zhuqinfei <1586859762@qq.com>
2021-08-30 01:06:25 +0800

    第二次封装设置多个请求头用到for in 循环

--- main.js
+++ main.js
@@ -11,9 +11,14 @@ window.jQuery.ajax=function(options){
   let body=options.body
   let successFn=options.successFn
   let failFn=options.failFn
+  let headers=options.headers
 
   let request=new XMLHttpRequest()
   request.open(method,url)  //新增请求网址
+  for(var key in headers){
+      value=headers[key]
+      request.setRequestHeader(key ,value)
+  }
   request.onreadystatechange=()=>{
     if(request.readyState === 4){
       if(request.status>=200 && request.status<300){
@@ -34,6 +39,10 @@ myButton.addEventListener('click',(e)=>{
   window.jQuery.ajax({
     url:'/xxx',
     method:'post',
+    headers:{
+      'Content-Type':'application/x-www-form-urlencoded',
+      'fei':'18'
+    },
     successFn:(x)=>{
       f1.call(undefined,x) //当我们有多个函数进行传参，就这样应用
       f2.call(undefined,x)
