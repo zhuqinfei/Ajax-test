7bd8b7759eb873bf0e34f91d545f1bba75041049
zhuqinfei <1586859762@qq.com>
2021-08-29 23:40:06 +0800

    第一次封装

--- main.js
+++ main.js
@@ -1,28 +1,32 @@
-myButton.addEventListener('click',(e)=>{
-  let request=new XMLHttpRequest()
-  request.open('get','http://jack.com:8002/xxx')  //新增请求网址
-  request.send()
-  request.onreadystatechange=()=>{
-    console.log(request.readyState)
-    if(request.readyState === 4){
-      console.log('请求响应都完毕了')
-      console.log(request.status)
-      if(request.status>=200 && request.status<300){
-         console.log('说明请求成功')
-         console.log(typeof request.responseText)
-         console.log(request.responseText)
-         let string = request.responseText
-         //符合JSON语法的字符串转换成JS对应的值
-         let object = window.JSON.parse(string) 
-         // JSON.parse 是浏览器提供的
-         console.log(typeof object)
-         console.log(object)
-         console.log('object.note')
-         console.log(object.note) 
-      }else if(request.status>=400){
-         console.log('说明请求失败') 
-      }
+window.jQuery=function(nodeOrSelector){
+   let nodes={}
+   nodes.addclass=function(){}
+   nodes.html=function(){}
+   return nodes
+}
 
+window.jQuery.ajax=function(url,method,body,successFn,failFn){
+  let request=new XMLHttpRequest()
+  request.open(method,url)  //新增请求网址
+  request.onreadystatechange=()=>{
+    if(request.readyState === 4){
+      if(request.status>=200 && request.status<300){
+       successFn.call(undefined,request.responseText) 
+      }else if(request.status>=400){
+       failFn.call(undefined,request) 
+      }
     }
   }
+  request.send(body)
+}
+window.$=window.jQuery
+
+myButton.addEventListener('click',(e)=>{
+  window.jQuery.ajax(
+    '/xxx',
+    'post',
+    'a=1&b=2',
+    (responseText)=>{console.log(1)},
+    (request)=>{console.log(2)}
+  )
 })
