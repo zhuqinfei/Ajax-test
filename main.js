7963a21d3f8c3e49000f1cf1d16c22c9fb246676
zhuqinfei <1586859762@qq.com>
2021-08-30 16:09:11 +0800

    第三次封装用到promise

--- main.js
+++ main.js
@@ -4,27 +4,30 @@ window.jQuery=function(nodeOrSelector){
    nodes.html=function(){}
    return nodes
 }
-
-window.jQuery.ajax=function({url,method,body,successFn,failFn,headers}){
-
-  let request=new XMLHttpRequest()
-  request.open(method,url)  
-  for(var key in headers){   // //设置多个请求头用for循环
-      value=headers[key]
-      request.setRequestHeader(key ,value)
-  }
-  request.onreadystatechange=()=>{
-    if(request.readyState === 4){
-      if(request.status>=200 && request.status<300){
-       successFn.call(undefined,request.responseText) 
-      }else if(request.status>=400){
-       failFn.call(undefined,request) 
-      }
-    }
-  }
-  request.send(body)
-}
 window.$=window.jQuery
+
+window.jQuery.ajax=function({url,method,body,headers}){
+  //成功和失败不用作为参数传进来，这里用resolve和reject统一化
+  //Ajax的promise返回return一般写在最上面，包裹着要执行的代码
+   return new Promise(function(resolve,reject){ //new 后面接大写
+     let request=new XMLHttpRequest()
+     request.open(method,url)  
+     for(var key in headers){   
+         value=headers[key]
+         request.setRequestHeader(key ,value)
+      }
+      request.onreadystatechange=()=>{
+       if(request.readyState === 4){
+          if(request.status>=200 && request.status<300){
+          resolve.call(undefined,request.responseText) 
+         }else if(request.status>=400){
+          reject.call(undefined,request) 
+         }
+       }
+     }
+     request.send(body)
+    })
+}
 
  function f1(responseText){} 
  function f2(responseText){}
@@ -33,14 +36,12 @@ myButton.addEventListener('click',(e)=>{
   window.jQuery.ajax({
     url:'/xxx',
     method:'post',
-    headers:{   //设置多个请求头
+    headers:{   
       'Content-Type':'application/x-www-form-urlencoded',
       'fei':'18'
     },
-    successFn:(x)=>{
-      f1.call(undefined,x) 
-      f2.call(undefined,x)
-    },
-    failFn:(x)=>{console.log(x)},
-  })
+  }).then(  //然后，成功调第一个函数，失败调第二个函数
+    (text)=>{console.log(text)},
+    (request)=>{console.log(request)}
+  )
 })
